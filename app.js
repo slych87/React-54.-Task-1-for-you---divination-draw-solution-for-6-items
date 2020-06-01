c37478ca4b1a3e9ebeb8bbb7347b418c223ab5d0
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-01 13:39:54 +0200

    lesson done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ class Draw extends React.Component {
   state = {
     options: ["1", "2", "3"],
     option: null,
+    value: "",
   }
 
   handleShowOption = () => {
@@ -17,7 +18,25 @@ class Draw extends React.Component {
     this.setState({
       option: this.state.options[index]
     })
+  }
 
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleAddOption = () => {
+
+    if (this.state.value === "") return alert('wpisz coś')
+    // const options = [...this.state.options]
+    // options.push(this.state.value)
+    const options = this.state.options.concat(this.state.value)
+    this.setState({
+      options,
+      value: '',
+    })
+    alert(`Wróżba dodana. Aktualne wróźby: ${options}`)
   }
 
   render() {
@@ -25,6 +44,10 @@ class Draw extends React.Component {
     return (
       <div>
         <button onClick={this.handleShowOption}>Zobacz wróżbę</button>
+        <br />
+        <input type="text" value={this.state.value} onChange={this.handleInputChange} />
+        <button onClick={this.handleAddOption}>Dodaj wróżbę</button>
+        <br />
         {this.state.option ? <h1>{this.state.option}</h1> : null}
       </div>
     )
